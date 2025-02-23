7c82166ed5fb8d21baae964d1c0abbe50b5d28b7
najifa6053 <esha15-6054@diu.edu.bd>
2025-02-23 11:09:52 +0600

    task02

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const Flutter());
 }
+
 
 class Flutter extends StatelessWidget {
   const Flutter({super.key});
@@ -16,15 +18,20 @@ class Flutter extends StatelessWidget {
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
-        body: Center(
-          child: Text(
-            "Advanced UI & Navigation",
-            style: TextStyle(
-              fontSize: 30,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl: 
+                    "https://as2.ftcdn.net/v2/jpg/11/17/87/31/1000_F_1117873193_M5pjieKHcp1szeSxu65jclh6NXR6DLN3.jpg",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+              
+            ],
             ),
-          ),
         ),
-      ),
-    );
+        ),
+      );
   }
 }
